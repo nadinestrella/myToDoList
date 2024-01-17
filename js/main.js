e9b029814845721c0d12dbb0ec1c1d99bab84750
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-01-17 10:29:56 +0100

    a

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 'use strict';
 
-const list = document.querySelector ('.js-list');
+const list = document.querySelector('.js-list');
 
 const tasks = [
-    { name: 'Recoger setas en el campo', completed: true, id:0 },
-    { name: 'Comprar pilas', completed: true, id:1 },
-    { name: 'Poner una lavadora de blancos', completed: true, id:2 },
-    { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript', completed: false, id:3
-    },
-  ];
+  { name: 'Recoger setas en el campo', completed: true, id: 0 },
+  { name: 'Comprar pilas', completed: true, id: 1 },
+  { name: 'Poner una lavadora de blancos', completed: true, id: 2 },
+  {
+    name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+    completed: false,
+    id: 3,
+  },
+];
 
-  // list.innerHTML = `<li><input type="checkbox"  id="input" name="input" class="task-input js-text-task-filter" checked><label for="input">Recoger setas en el campo</label></li>`;
+// list.innerHTML = `<li><input type="checkbox"  id="input" name="input" class="task-input js-text-task-filter" checked><label for="input">Recoger setas en el campo</label></li>`;
 /*
   list.innerHTML = 
   `<li><input type="checkbox" 
@@ -38,72 +41,64 @@ const tasks = [
    <label for="input"> ${tasks[3].name} </label></li>`
    
 */
-  
-//NO ENTIENDO PARA QUE QUEREMOS EL TASK ID. PORQUE SALE IGUAL PINTADO 
 
- function renderList () {
-  for (const task of tasks){
-  
-  if(task.completed) {
-    list.innerHTML += 
-    `<li class='tachado js-li' name='${task.id}'>${task.name}<input 
+//NO ENTIENDO PARA QUE QUEREMOS EL TASK ID. PORQUE SALE IGUAL PINTADO
+
+function renderList() {
+  for (const task of tasks) {
+    if (task.completed) {
+      list.innerHTML += `<li class='tachado js-li' name='${task.id}'>${task.name}<input 
     type="checkbox" 
     id="${task.id}" 
     name="input" 
     class="task-input js-text-task-filter" checked> </li>
-    `
-  } else { 
-    list.innerHTML +=  `<li class='js-li' name='${task.id}'>${task.name}<input 
+    `;
+    } else {
+      list.innerHTML += `<li class='js-li' name='${task.id}'>${task.name}<input 
   type="checkbox" 
   id="${task.id}" 
   name="input" 
   class="task-input js-text-task-filter" > </li>
-  `
+  `;
+    }
+  }
+  //escuchar eventos del li
+  const li = document.querySelectorAll('.js-li'); //li es un array
+  for (const eachli of li) {
+    eachli.addEventListener('click', handleClick);
   }
 }
-//escuchar eventos del li
-const li = document.querySelectorAll ('.js-li'); //li es un array 
-  for (const eachli of li){
-    eachli.addEventListener('click', handleClick);
-    
-  }
- };
 
- function handleClick (event) {
-  console.log (event.currentTarget);
-  console.log (event.target);
+function handleClick(event) {
+  console.log(event.currentTarget);
+  console.log(event.target);
+}
 
- }
+renderList();
 
-
-  renderList();
- 
-  //2 FILTRAR TAREAS
+//2 FILTRAR TAREAS
 
 const btnFilter = document.querySelector('.js-btn-filter');
 const textTaskFilter = document.querySelector('.js-text-task-filter');
 const filteredTask = [];
 
-function handleFilter (event) {
+function handleFilter(event) {
   event.preventDefault();
-  
+
   /*list.innerHTML += `<li> ${textTaskFilter.value}</li>`;*/
-  
+
   console.log(textTaskFilter.value);
 
   for (const task of tasks) {
-    const inputFill = textTaskFilter.value
+    const inputFill = textTaskFilter.value;
     if (task.name.includes(inputFill)) {
-      filteredTask.push (task);
+      filteredTask.push(task);
       list.innerHTML += `<li> ${filteredTask.name} </li>`;
-    
-      
-      console.log (filteredTask);
 
-  } 
+      console.log(filteredTask);
+    }
+  }
 }
-
-};
 
 btnFilter.addEventListener('click', handleFilter);
 
